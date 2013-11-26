367c1d608ecbcba6e062cbb420e2c28bcff6e8c5
Peter Mumford <peter@petermumford.com>
2013-11-26 18:50:58 +0000

    fetch weather data from forecast and cache results in redis

--- forecast_ruby.gemspec
+++ forecast_ruby.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.required_ruby_version = '>= 1.8.7'
 
-  s.add_dependency 'httparty'
+  spec.add_dependency 'httparty'
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
--- lib/forecast_ruby.rb
+++ lib/forecast_ruby.rb
@@ -1,5 +1,52 @@
 require "forecast_ruby/version"
+require 'forecast_ruby/configuration'
+require 'forecast_ruby/helpers'
+require 'httparty'
 
 module ForecastRuby
-  # Your code goes here...
+	extend Configuration
+	extend Helpers
+
+  class << self
+  	def forecast(lat, lng)
+  		@latitude = lat
+  		@longitude = lng
+			@redis_forecast_key = "forecast:location:#{@latitude}:#{@longitude}"
+
+  		if forecast_location_exists?
+        get_forecast_location
+      else
+	  		response = get_forcast(request_url)
+				set_forecast_location(response)
+				response
+	    end
+  	end
+
+	  private
+	  	def get_forcast(query)
+				HTTParty.get(query)
+	  	end
+
+	  	def request_url
+	  		ForecastRuby.api_endpoint + query_string
+	  	end
+
+	  	def query_string
+	  		"#{ForecastRuby.api_key}/#{@latitude},#{@longitude}?units=#{ForecastRuby.units}"
+	  	end
+
+			# Redis Methods
+			def forecast_location_exists?
+			  $redis.exists(@redis_forecast_key)
+			end
+
+			def set_forecast_location(response)
+				$redis.set(@redis_forecast_key, response.to_json)
+				$redis.expire(@redis_forecast_key, 14400) #in seconds = 4 hours
+			end
+
+			def get_forecast_location
+			  $redis.get(@redis_forecast_key)
+			end
+  end
 end
